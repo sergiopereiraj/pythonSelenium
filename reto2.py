d6e8b470614c4a07c12028bceca32f5563b791d9
Sergio Pereira Jorquera <sergiopereira.jorquera@gmail.com>
2022-04-22 11:32:41 -0400

    guardar

--- reto2.py
+++ reto2.py
@@ -1,7 +1,8 @@
 from lib2to3.pgen2 import driver
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import json 
+import json
+import re
 
 url = 'https://www.sii.cl/servicios_online/1047-nomina_inst_financieras-1714.html'
 
@@ -11,8 +12,8 @@ driver.get(url)
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-tables = soup.find_all('table')
-for table in tables:
-    tablestxt = table.get_text()
-    print(table.next_sibling)
-print(tables)
+tables = soup.find_all('td') #busca todos los td y los trae en forma de lista
+# for table in tables:
+#     tablestxt = table.get_text()
+#     print(table.next_sibling)
+# limpiar = re.sub(r"/[\<\>\/]/g", tables)

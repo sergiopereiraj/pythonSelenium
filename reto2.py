16187ac049f8b56a69e648f0b69d80136c245baa
Sergio Pereira Jorquera <sergiopereira.jorquera@gmail.com>
2022-04-25 12:20:06 -0400

    ultimo

--- reto2.py
+++ reto2.py
@@ -1,5 +1,5 @@
-from lib2to3.pgen2 import driver
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import json
 
@@ -11,16 +11,19 @@ driver.get(url)
 
 driver.maximize_window()
 
-ides = driver.find_elements_by_xpath('//tbody/tr/td[1]')
-razonSociales = driver.find_elements_by_xpath('//tbody/tr/td[2]')
-paises = driver.find_elements_by_xpath('//tbody/tr/td[3]')
-datos = driver.find_elements_by_xpath('//tbody/tr/td[4]')
-vigencias = driver.find_elements_by_xpath('//tbody/tr/td[5]')
-actualizaciones = driver.find_elements_by_xpath('//tbody/tr/td[6]')
-estados = driver.find_elements_by_xpath('//tbody/tr/td[7]')
+# columnas = driver.find_element_by_xpath('//thead/tr/th')
+
+# ides = driver.find_elements_by_xpath('//tbody/tr/td[1]')
+ides = driver.find_elements(By.XPATH,'//tbody/tr/td[1]')
+razonSociales = driver.find_elements(By.XPATH,'//tbody/tr/td[2]')
+paises = driver.find_elements(By.XPATH,'//tbody/tr/td[3]')
+datos = driver.find_elements(By.XPATH,'//tbody/tr/td[4]')
+vigencias = driver.find_elements(By.XPATH,'//tbody/tr/td[5]')
+actualizaciones = driver.find_elements(By.XPATH,'//tbody/tr/td[6]')
+estados = driver.find_elements(By.XPATH,'//tbody/tr/td[7]')
 
 lista = []
-for i in range(156):
+for i in range(len(ides)):
     temporaly_data = {"No": ides[i].text, "RAZÓN SOCIAL": razonSociales[i].text, "PAÍS": paises[i].text, "DATOS INSCRIPCIÓN": datos[i].text, "VIGENCIA HASTA": vigencias[i].text, "DATOS ÚLTIMA ACTUALIZACIÓN":actualizaciones[i].text, "ESTADO": estados[i].text}
     lista.append(temporaly_data)
 
@@ -31,7 +34,7 @@ data_json = json.dumps(lista, ensure_ascii=False)
 print(data_json)
 
 
-#     temporaly_data = {'No': ides[i].text ['RAZÓN SOCIAL': razonSociales[i].text, 'PAÍS': paises[i].text, 'DATOS INSCRIPCIÓN': datos[i].text, 'VIGENCIA HASTA': vigencias[i].text, 'DATOS ÚLTIMA ACTUALIZACIÓN':actualizaciones[i].text, 'ESTADO': estados[i].text]}
+#     temporaly_data = {ides[i].text : ['RAZÓN SOCIAL': razonSociales[i].text, 'PAÍS': paises[i].text, 'DATOS INSCRIPCIÓN': datos[i].text, 'VIGENCIA HASTA': vigencias[i].text, 'DATOS ÚLTIMA ACTUALIZACIÓN':actualizaciones[i].text, 'ESTADO': estados[i].text]}
 
 # for estado in estados:
 #     print(estado.text)
